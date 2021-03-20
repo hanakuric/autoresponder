68cdaf2074b091c94a7d04cc8e531a76c0e303ee
Hana Kuric <hana.kuric@stu.ibu.edu.ba>
2021-03-20 18:18:59 +0100

    edited and added config.php

---
+++ api/config.php
@@ -0,0 +1,12 @@
+<?php
+
+class Config {
+
+  const DB_HOST = "localhost";
+  const DB_USERNAME = "root";
+  const DB_PASSWORD = "root123";
+  const DB_SCHEME = "autoresponder";
+
+}
+
+?>
--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -1,9 +1,21 @@
 <?php
 
+require_once dirname(__FILE__)."/../config.php";
+
 class BaseDao{
+
+private $connection;
 
   public function __construct(){
 
+
+    try {
+      $this->connection = new PDO("mysql:host=".Config::DB_HOST.";dbname=".Config::DB_SCHEME, Config::DB_USERNAME, Config::DB_PASSWORD);
+      // set the PDO error mode to exception
+      $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
 
   }
 
@@ -18,7 +30,7 @@ class BaseDao{
   }
 
   public function query(){
-
+// SELECT * FROM 'users' WHERE id = 7;
 
   }
 
