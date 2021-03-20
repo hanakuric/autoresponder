32bcf1b84eaa5ba494e94a83ef0d1324ef7b17e2
Hana Kuric <hana.kuric@stu.ibu.edu.ba>
2021-03-21 00:04:37 +0100

    edited test.php

--- api/test.php
+++ api/test.php
@@ -8,7 +8,14 @@ require_once dirname(__FILE__)."/dao/UserDao.class.php";
 
 $user_dao = new UserDao();
 
-$user = $user_dao->get_user_by_id(5); 
+$user1 = [
+"name" => "Saliha Kuric",
+"email" => "saliha.kuric@gmail.com",
+"password" => "1234567",
+"account_id" => 1
+];
+$user = $user_dao->add_user($user1);
+//$user = $user_dao->get_user_by_id(5);
 
 //$user_dao->get_user_by_email("hanakuric@gmail.com");
 
