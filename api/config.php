e07829d211e8688a0d855a16a1c29c11f9403d4f
Hana Kuric <hana.kuric@stu.ibu.edu.ba>
2021-03-20 18:33:59 +0100

    edited config

--- api/config.php
+++ api/config.php
@@ -3,8 +3,8 @@
 class Config {
 
   const DB_HOST = "localhost";
-  const DB_USERNAME = "root";
-  const DB_PASSWORD = "root123";
+  const DB_USERNAME = "autoresponder";
+  const DB_PASSWORD = "autoresponder";
   const DB_SCHEME = "autoresponder";
 
 }
